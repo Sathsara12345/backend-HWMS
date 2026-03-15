6d3be1102a2aeaa75b9762935834b153c1b2cb44
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-03-15 11:04:00 +0530

    Refactor API routes to improve organization of admin and user management functionalities. Introduce a dedicated middleware group for user management under the super-admin prefix, enhancing clarity and maintainability of the route structure.

--- routes/api.php
+++ routes/api.php
@@ -17,24 +17,24 @@ Route::prefix('v1')->group(function () {
 
         // Admin routes
         Route::middleware(['role:super-admin|admin'])->prefix('admin')->group(function () {
-            Route::get('/dashboard', [AdminDashboardController::class , 'index']);         
-        });
-
-        Route::middleware(['auth:api', 'role:super-admin'])->prefix('super-admin')->group(function () {
-            // Roles and Permissions routes
-            Route::apiResource('roles', RoleController::class);
-            Route::get('all-permissions', [RoleController::class, 'getAllPermissions']);
-            Route::post('roles/{role}/assign-permissions', [RoleController::class, 'assignPermissions']);
-            Route::apiResource('permissions', PermissionController::class);
-            Route::apiResource('users', UserController::class);
-            Route::post('/roles/assign-permissions-to-user/{userId}', [RoleController::class, 'assignPermissionsToUser']);
-
-            // Merchant routes
+            Route::get('/dashboard', [AdminDashboardController::class , 'index']);
+            
+            // Merchant paths (Commonly used by admins too)
             Route::get('/merchants', [MerchantController::class, 'index']);
             Route::get('/merchants/{id}', [MerchantController::class, 'show']);
             Route::post('/merchants', [MerchantController::class, 'store']);
             Route::put('/merchants/{id}', [MerchantController::class, 'update']);
             Route::delete('/merchants/{id}', [MerchantController::class, 'destroy']);
+
+            // User management
+            Route::middleware(['role:super-admin'])->group(function () {
+                Route::apiResource('roles', RoleController::class);
+                Route::get('all-permissions', [RoleController::class, 'getAllPermissions']);
+                Route::post('roles/{role}/assign-permissions', [RoleController::class, 'assignPermissions']);
+                Route::apiResource('permissions', PermissionController::class);
+                Route::apiResource('users', UserController::class);
+                Route::post('/roles/assign-permissions-to-user/{userId}', [RoleController::class, 'assignPermissionsToUser']);
+            });
         });
     });
 });
