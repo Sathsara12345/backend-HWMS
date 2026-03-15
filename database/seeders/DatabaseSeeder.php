dbb7f6ed594d11fcba04be329a1248dd80ead9bc
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-03-15 23:23:38 +0530

    Add WebsiteStructureSeeder and MerchantSeeder to DatabaseSeeder for improved data initialization. This ensures default roles, website structure, and merchant data are seeded during the database setup process, enhancing the overall seeding efficiency.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesSeeder::class,
+            WebsiteStructureSeeder::class,
             RolesAndPermissionsSeeder::class,
+            MerchantSeeder::class,
         ]);
     }
 }
